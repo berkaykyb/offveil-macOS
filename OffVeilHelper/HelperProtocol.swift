819e669c0b344d8db50e4e1e9997cedc8177c161
berkaykyb <berkayabasi2003@hotmail.com>
2026-02-14 02:20:41 +0300

    remove unused app-side helper layer

--- OffVeilHelper/HelperProtocol.swift
+++ OffVeilHelper/HelperProtocol.swift
@@ -1,1 +1,12 @@
-../OffVeil/Services/HelperProtocol.swift
+//
+//  HelperProtocol.swift
+//  OffVeilHelper
+//
+//  Created by Berkay KAYABAŞI on 5.02.2026.
+//
+
+import Foundation
+
+@objc protocol HelperProtocol {
+    func executeCommand(_ command: String, withReply reply: @escaping (Bool, String) -> Void)
+}
